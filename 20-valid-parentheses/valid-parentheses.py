25e258011293d68453434696f123b176cea7326e
Ellie Ha <168208159+ellieryus@users.noreply.github.com>
2025-10-08 16:40:54 -0400

    Time: 3 ms (71.98%) | Memory: 12.7 MB (29.62%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,13 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        mapping = {")": "(", "}": "{", "]": "["}
+        # mapping = {")": "(", "}": "{", "]": "["}
+        # stack = []
+        # for char in s:
+        #     if char in mapping:
+        #         top_element = stack.pop() if stack else '#'
+        #         if mapping[char] != top_element:
+        #             return False
+        #     else:
+        #         stack.append(char)
+        # return not stack    
+
+        mapping = {"(": ")", "{": "}", "[": "]"}
         stack = []
         for char in s:
             if char in mapping:
-                top_element = stack.pop() if stack else '#'
-                if mapping[char] != top_element:
-                    return False
-            else:
-                stack.append(char)
-        return not stack    
+                stack.append(mapping[char])  
+            elif not stack or stack.pop() != char:
+                return False
+        return not stack
+
